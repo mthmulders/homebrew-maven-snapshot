814e34570dd347250e4fdf8deed6d56ba11e8103
Maarten Mulders <mthmulders@noreply.github.com>
2022-09-18 22:40:21 +0000

    Update to latest Maven snapshot

--- Formula/maven-snapshot.rb
+++ Formula/maven-snapshot.rb
@@ -1,11 +1,11 @@
 class MavenSnapshot < Formula
   desc "Java-based project management (latest snapshot)"
   homepage "https://maven.apache.org/"
-  url "https://ci-maven.apache.org/job/Maven/job/maven-box/job/maven/job/master/89/artifact/org/apache/maven/apache-maven/4.0.0-alpha-1-SNAPSHOT/apache-maven-4.0.0-alpha-1-SNAPSHOT-bin.tar.gz"
+  url "https://ci-maven.apache.org/job/Maven/job/maven-box/job/maven/job/master/91/artifact/org/apache/maven/apache-maven/4.0.0-alpha-1-SNAPSHOT/apache-maven-4.0.0-alpha-1-SNAPSHOT-bin.tar.gz"
   version "4.0.0-alpha-1-SNAPSHOT"
-  sha256 "b964a3556ca0491781b9c3b4cdba388efa3092a0bebcd9eab56e5eed5074cf24"
+  sha256 "96d645a6eea09abe49eadbcf7b2b80b584751e70063540f6c6d561f93ff4db67"
   license "Apache-2.0"
-  revision 417
+  revision 419
 
   depends_on "openjdk"
 
